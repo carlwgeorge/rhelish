ead87070f3d4c09f59c90c8ae11f38133c42aa85
Carl George <carl.george@rackspace.com>
2016-07-16 01:05:05 -0500

    add info flag to display links

--- src/rhelish/__init__.py
+++ src/rhelish/__init__.py
@@ -6,8 +6,19 @@ from rhelish import pkgdb
 @click.command()
 @click.argument('package')
 @click.option('--search', '-s', is_flag=True)
-def cli(package, search):
+@click.option('--info', '-i', is_flag=True)
+def cli(package, search, info):
     if search:
         # search Fedora pkgdb for package
         results = pkgdb.name_search(package)
         click.echo('\n'.join(results))
+    elif info:
+        # display helpful links for package
+        urls = [
+            'http://pkgs.fedoraproject.org/cgit/{}.git'.format(package),
+            'https://admin.fedoraproject.org/pkgdb/package/{}'.format(package),
+            'https://bodhi.fedoraproject.org/updates/?packages={}'.format(package),
+            'http://koji.fedoraproject.org/koji/search?type=package&match=glob&terms={}'.format(package),
+            'https://bugzilla.redhat.com/buglist.cgi?bug_status=NEW&bug_status=ASSIGNED&bug_status=ON_QA&component={}'.format(package)
+        ]
+        click.echo('\n'.join(urls))
